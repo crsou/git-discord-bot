d6385f6c4d3f8ea4b16d9c24018540236bfb1514
Carlos Souza <cvictor.rsouza@gmail.com>
2021-05-14 12:39:56 -0300

    test embed return & webhook id reading (3)

--- index.js
+++ index.js
@@ -8,16 +8,13 @@ client.once("ready", () => {
 });
 
 client.on("message", (message) => {
-  if (message.embeds.length > 0 && message.author.username === "GitHub") {
+  if (message.webhookID && message.author.username === "GitHub") {
     console.log(message);
     message.channel.send("Github update!");
     message.channel.send(`Repo: ${message.embeds[0].title}`);
     message.channel.send(`Commit: ${message.embeds[0].description}`);
     message.channel.send("Embed:");
     message.channel.send({ embed: message.embeds[0] });
-  }
-  if (message.webhookID) {
-    console.log("this works too");
   }
 });
 
